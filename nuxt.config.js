2893a16b251120e7cc14d9f9a8a6d990a7d63d61
jin35314031 <jin35314031@gmail.com>
2020-08-04 13:21:35 +0900

    displayList

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,12 +52,21 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
+    '@nuxtjs/proxy'
   ],
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
-  axios: {},
+  axios: {
+    proxy: true
+  },
+  proxy:{
+    '/api/':{
+      target:'http://localhost:8080/employees',
+      pathRewrite: {'^/api/': ''},
+    }
+  },
   /*
   ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,24 @@
 <template>
   <div class="container">
-    {{users}}
-    <button v-on:click=""></button>
+    <p>{{ $store.state.employees.employees }}</p>
+    <!-- <button v-on:click="$store.dispatch('')">Delete</button> -->
+
   </div>
 </template>
 
 <script>
-//const axios = require('axios')
-//let url = 'https://api.github.com/users/jin35314031'
-//let url = 'http://localhost:8080/employees'
-
-//export default {
-//  asyncData({ params }) {
-//    return axios.get(url)
-//      .then ((res) => {
-//        return { users: res.data }
-//      })
-//  }
-//}
-
-import axios from 'axios'
 
 export default {
-async asyncData ({ params }) {
-  const { data: users } = await axios.get(`http://localhost:8080/employees`)
-  return {
-    users
-  }
+
+mounted: function () {
+  this.$store.dispatch('employees/updateEmployeesAction')
  }
 }
 
 
-
 </script>
+
+
+
+
 
